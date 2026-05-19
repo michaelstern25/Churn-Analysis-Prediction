12f9719d239884ccee3eb71c47cd8848c4f3cf63
Arman khan <armankhan271609@gmail.com>
2026-05-19 15:15:28 +0530

    Add files via upload

---
+++ Customer Churn SQL Analysis.sql
@@ -0,0 +1,195 @@
+CREATE TABLE customer_churn (
+    Customer_ID VARCHAR(20) PRIMARY KEY,
+    Gender VARCHAR(10),
+    Age VARCHAR(50),
+    Married VARCHAR(10),
+    State VARCHAR(50),
+    Number_of_Referrals VARCHAR(50),
+    Tenure_in_Months VARCHAR(50),
+    Value_Deal VARCHAR(50),
+	Phone_Service VARCHAR(50),
+	Multiple_Lines VARCHAR(50),
+	Internet_Service VARCHAR(50),
+	Internet_Type VARCHAR(50),
+	Online_Security VARCHAR(50),
+	Online_Backup VARCHAR(50),
+	Device_Protection_Plan VARCHAR(50),
+	Premium_Support VARCHAR(50),
+	Streaming_TV VARCHAR(50),
+	Streaming_Movies VARCHAR(50),
+	Streaming_Music VARCHAR(50),
+	Unlimited_Data VARCHAR(50),
+	Contract VARCHAR(50),
+	Paperless_Billing VARCHAR(50),
+	Payment_Method VARCHAR(50),
+	Monthly_Charge VARCHAR(50),
+	Total_Charges VARCHAR(50),
+	Total_Refunds VARCHAR(50),
+	Total_Extra_Data_Charges VARCHAR(50),
+	Total_Long_Distance_Charges VARCHAR(50),
+	Total_Revenue VARCHAR(50),
+	Customer_Status VARCHAR(50),
+	Churn_Category VARCHAR(50),
+	Churn_Reason VARCHAR(50)
+);
+
+SELECT * FROM customer_churn;
+
+-- Calculate total male and female customers
+-- and find their percentage from total customers
+SELECT 
+	Gender, 
+	COUNT(Gender) AS TotalCount,
+    ROUND (COUNT(Gender) * 100.0 / (SELECT COUNT(*) FROM customer_churn), 2) AS Percentage
+FROM customer_churn
+GROUP BY Gender;
+
+
+-- Calculate total customers for each contract type
+-- and find their percentage from total customers
+SELECT 
+	Contract, 
+	COUNT(Contract) AS TotalCount,
+	ROUND (COUNT(Contract) * 100.0 / (SELECT COUNT(*) FROM customer_churn), 2)  AS Percentage
+from customer_churn
+Group by Contract;
+
+-- Change Data Type.
+ALTER TABLE customer_churn
+ALTER COLUMN total_revenue TYPE float
+USING total_revenue::float;
+
+
+-- Calculate total customers and total revenue
+-- for each customer status category
+SELECT 
+    Customer_Status,
+	-- Count total customers
+    COUNT(Customer_Status) AS TotalCount,
+	-- Calculate total revenue
+    SUM(total_revenue) AS TotalRev,
+
+	 -- Calculate revenue percentage
+    SUM(Total_Revenue) * 100.0 /
+    (SELECT SUM(Total_Revenue) FROM customer_churn)
+    AS RevPercentage
+	
+FROM customer_churn
+GROUP BY Customer_Status;
+
+
+
+-- This query analyzes customer distribution by state
+SELECT 
+    State, 
+    Count(State) as TotalCount,
+    Count(State) * 100.0 / (Select Count(*) from customer_churn)  as Percentage
+from customer_churn
+Group by State
+Order by Percentage desc;
+
+
+-- Check Unique Values.
+SELECT DISTINCT Internet_Type
+FROM customer_churn;
+
+SELECT 
+    DISTINCT Internet_Type,
+    COUNT(Internet_Type) AS TotalCount
+FROM customer_churn
+GROUP BY Internet_Type;
+
+
+-- Count Null values for each columns.
+SELECT 
+    SUM(CASE WHEN Customer_ID IS NULL THEN 1 ELSE 0 END) AS Customer_ID_Null_Count,
+    SUM(CASE WHEN Gender IS NULL THEN 1 ELSE 0 END) AS Gender_Null_Count,
+    SUM(CASE WHEN Age IS NULL THEN 1 ELSE 0 END) AS Age_Null_Count,
+    SUM(CASE WHEN Married IS NULL THEN 1 ELSE 0 END) AS Married_Null_Count,
+    SUM(CASE WHEN State IS NULL THEN 1 ELSE 0 END) AS State_Null_Count,
+    SUM(CASE WHEN Number_of_Referrals IS NULL THEN 1 ELSE 0 END) AS Number_of_Referrals_Null_Count,
+    SUM(CASE WHEN Tenure_in_Months IS NULL THEN 1 ELSE 0 END) AS Tenure_in_Months_Null_Count,
+    SUM(CASE WHEN Value_Deal IS NULL THEN 1 ELSE 0 END) AS Value_Deal_Null_Count,
+    SUM(CASE WHEN Phone_Service IS NULL THEN 1 ELSE 0 END) AS Phone_Service_Null_Count,
+    SUM(CASE WHEN Multiple_Lines IS NULL THEN 1 ELSE 0 END) AS Multiple_Lines_Null_Count,
+    SUM(CASE WHEN Internet_Service IS NULL THEN 1 ELSE 0 END) AS Internet_Service_Null_Count,
+    SUM(CASE WHEN Internet_Type IS NULL THEN 1 ELSE 0 END) AS Internet_Type_Null_Count,
+    SUM(CASE WHEN Online_Security IS NULL THEN 1 ELSE 0 END) AS Online_Security_Null_Count,
+    SUM(CASE WHEN Online_Backup IS NULL THEN 1 ELSE 0 END) AS Online_Backup_Null_Count,
+    SUM(CASE WHEN Device_Protection_Plan IS NULL THEN 1 ELSE 0 END) AS Device_Protection_Plan_Null_Count,
+    SUM(CASE WHEN Premium_Support IS NULL THEN 1 ELSE 0 END) AS Premium_Support_Null_Count,
+    SUM(CASE WHEN Streaming_TV IS NULL THEN 1 ELSE 0 END) AS Streaming_TV_Null_Count,
+    SUM(CASE WHEN Streaming_Movies IS NULL THEN 1 ELSE 0 END) AS Streaming_Movies_Null_Count,
+    SUM(CASE WHEN Streaming_Music IS NULL THEN 1 ELSE 0 END) AS Streaming_Music_Null_Count,
+    SUM(CASE WHEN Unlimited_Data IS NULL THEN 1 ELSE 0 END) AS Unlimited_Data_Null_Count,
+    SUM(CASE WHEN Contract IS NULL THEN 1 ELSE 0 END) AS Contract_Null_Count,
+    SUM(CASE WHEN Paperless_Billing IS NULL THEN 1 ELSE 0 END) AS Paperless_Billing_Null_Count,
+    SUM(CASE WHEN Payment_Method IS NULL THEN 1 ELSE 0 END) AS Payment_Method_Null_Count,
+    SUM(CASE WHEN Monthly_Charge IS NULL THEN 1 ELSE 0 END) AS Monthly_Charge_Null_Count,
+    SUM(CASE WHEN Total_Charges IS NULL THEN 1 ELSE 0 END) AS Total_Charges_Null_Count,
+    SUM(CASE WHEN Total_Refunds IS NULL THEN 1 ELSE 0 END) AS Total_Refunds_Null_Count,
+    SUM(CASE WHEN Total_Extra_Data_Charges IS NULL THEN 1 ELSE 0 END) AS Total_Extra_Data_Charges_Null_Count,
+    SUM(CASE WHEN Total_Long_Distance_Charges IS NULL THEN 1 ELSE 0 END) AS Total_Long_Distance_Charges_Null_Count,
+    SUM(CASE WHEN Total_Revenue IS NULL THEN 1 ELSE 0 END) AS Total_Revenue_Null_Count,
+    SUM(CASE WHEN Customer_Status IS NULL THEN 1 ELSE 0 END) AS Customer_Status_Null_Count,
+    SUM(CASE WHEN Churn_Category IS NULL THEN 1 ELSE 0 END) AS Churn_Category_Null_Count,
+    SUM(CASE WHEN Churn_Reason IS NULL THEN 1 ELSE 0 END) AS Churn_Reason_Null_Count
+FROM customer_churn;
+
+
+
+-- Remove null and insert the new data into Prod table.
+CREATE TABLE prod_churn AS
+SELECT 
+    Customer_ID,
+    Gender,
+    Age,
+    Married,
+    State,
+    Number_of_Referrals,
+    Tenure_in_Months,
+    COALESCE(Value_Deal, 'None') AS Value_Deal,
+    Phone_Service,
+    COALESCE(Multiple_Lines, 'No') AS Multiple_Lines,
+    Internet_Service,
+    COALESCE(Internet_Type, 'None') AS Internet_Type,
+    COALESCE(Online_Security, 'No') AS Online_Security,
+    COALESCE(Online_Backup, 'No') AS Online_Backup,
+    COALESCE(Device_Protection_Plan, 'No') AS Device_Protection_Plan,
+    COALESCE(Premium_Support, 'No') AS Premium_Support,
+    COALESCE(Streaming_TV, 'No') AS Streaming_TV,
+    COALESCE(Streaming_Movies, 'No') AS Streaming_Movies,
+    COALESCE(Streaming_Music, 'No') AS Streaming_Music,
+    COALESCE(Unlimited_Data, 'No') AS Unlimited_Data,
+    Contract,
+    Paperless_Billing,
+    Payment_Method,
+    Monthly_Charge,
+    Total_Charges,
+    Total_Refunds,
+    Total_Extra_Data_Charges,
+    Total_Long_Distance_Charges,
+    Total_Revenue,
+    Customer_Status,
+    COALESCE(Churn_Category, 'Others') AS Churn_Category,
+    COALESCE(Churn_Reason, 'Others') AS Churn_Reason
+FROM customer_churn;
+
+SELECT * FROM prod_churn;
+
+-- I created SQL views to provide filtered datasets for Power BI reporting. 
+-- Separate views were built for churn analysis and new joiner analysis to 
+-- simplify dashboard development and improve query reusability.
+
+CREATE VIEW vw_ChurnData AS
+SELECT *
+FROM prod_churn
+WHERE Customer_Status IN ('Churned', 'Stayed');
+
+
+CREATE VIEW vw_JoinData AS
+SELECT *
+FROM prod_churn
+WHERE Customer_Status = 'Joined';
+
+SELECT * FROM vw_churndata;
